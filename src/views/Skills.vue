e60ba20f6e0fd6f2db1d64556638f851e7cbe596
urape <mydevelop.mkmk@gmail.com>
2019-11-01 16:21:51 +0900

    modify skills

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -2,20 +2,24 @@
     <b-container class="box">
       <h1>Skills</h1>
       <b-row class="content">
-        <b-col md="1"></b-col>
-        <b-col md="5" cols="12">
+        <b-col md="4" cols="12">
           <ul id="lang1">
             <h3>Lang</h3>
             <li v-for="lang in langs" :key="lang.id">{{ lang.name }}</li>
           </ul>
         </b-col>
-        <b-col md="5" cols="12">
+        <b-col md="4" cols="12">
           <ul>
             <h3>Framework</h3>
             <li v-for="framework in frameworks" :key="framework.id">{{ framework.name }}</li>
           </ul>
         </b-col>
-        <b-col md="1"></b-col>
+        <b-col md="4" cols="12">
+          <ul>
+            <h3>Other</h3>
+            <li v-for="other in others" :key="other.id">{{ other.name }}</li>
+          </ul>
+        </b-col>
       </b-row>
     </b-container>
 </template>
@@ -26,14 +30,20 @@ export default {
     return {
       langs: [
         { name: 'PHP' },
+        { name: 'JavaScript' },
+        { name: 'jQuery' },
         { name: 'HTML' },
         { name: 'CSS' },
-        { name: 'ACCESS VBA' }
+        { name: 'MySQL' }
       ],
       frameworks: [
         { name: 'Laravel' },
         { name: 'CodeIgniter' },
         { name: 'Bootstrap' },
+      ],
+      others: [
+        { name: 'Git' },
+        { name: 'ACCESS VBA' }
       ]
     }
   }
