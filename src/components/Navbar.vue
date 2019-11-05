3b32afe584bf2b6b6d13308256c616f7a0630eb2
urape <mydevelop.mkmk@gmail.com>
2019-11-05 18:41:45 +0900

    add fortawesome icon

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,28 @@
 <template>
-    <header>
-      <nav id="nav">
-        <router-link to="/">Top</router-link> |
-        <router-link to="/careers">Careers</router-link> |
-        <router-link to="/skills">Skills</router-link> |
-        <router-link to="/works">Works</router-link>
-      </nav>
-    </header>
+  <nav id="nav">
+    <router-link to="/">Top</router-link> |
+    <router-link to="/careers">Careers</router-link> |
+    <router-link to="/skills">Skills</router-link> |
+    <router-link to="/works">Works</router-link>
+    <div class="d-inline icon">
+      <a href="https://github.com/urape/portfolio"><font-awesome-icon :icon="['fab', 'github-square']"/></a>
+    </div>
+  </nav>
+  
 </template>
 
 <style scoped>
-header {
-    width: 100%;
-    margin-bottom: 15px;
-    height: 30px;
-    top: 0;
-    left: 0;
-    position: fixed;
-    z-index: 1;
-    font-size: 20px;
-    background: linear-gradient(-90deg, #84CF6A, #16C0B0);
+#nav {
+  position: relative;
+  width: 100%;
+  margin-bottom: 15px;
+  height: 30px;
+  top: 0;
+  left: 0;
+  position: fixed;
+  z-index: 1;
+  font-size: 20px;
+  background: linear-gradient(-90deg, #84CF6A, #16C0B0);
 }
 
 #nav a {
@@ -30,5 +33,11 @@ header {
 
 #nav a.router-link-exact-active {
   color: #FFFFFF;
+}
+
+.icon {
+  position: absolute;
+  font-size: 22px;
+  right: 2%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fab)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 import router from './router'
 
 Vue.config.productionTip = false
